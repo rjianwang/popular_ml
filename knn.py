f5cafab2f6d3d84c4027e9b179b04d14c642fc84
rjianwang <rjianwang@foxmail.com>
2016-04-27 19:28:23 +0800

    add knn.py

---
+++ knn.py
@@ -0,0 +1,36 @@
+import numpy as np
+import operator
+
+def kNNClassify(x, X, y, k = 3):
+    n_samples, n_features = X.shape
+
+    # calculate euclidean distances
+    diff = np.tile(x, (n_samples, 1)) - X
+    squaredDiff = diff**2
+    squaredDist = np.sum(squaredDiff, axis = 1)
+    distance = squaredDist**0.5
+
+    sortedDistIndices = np.argsort(distance)
+
+    classCount = {}
+    for i in range(k):
+        voteLabel = y[sortedDistIndices[i]]
+        classCount[voteLabel] = classCount.get(voteLabel, 0) + 1
+
+    sortedClassCount = sorted(classCount.iteritems(),
+            key = operator.itemgetter(1), reverse = True)
+
+    return sortedClassCount[0][0]
+
+
+
+def test():
+    X = np.array([[1, 1], [1, 2], [2, 2], [6, 4], [6, 5], [6, 6]])
+    y = np.array([0, 0, 0, 1, 1, 1])
+    x = np.array([[7, 1]])
+    k = 3
+
+    print kNNClassify(x, X, y, k)
+
+if __name__ == '__main__':
+    test()
