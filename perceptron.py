6edcfa929fd3eab2758f111b2d80dbb501048f80
rjianwang <rjianwang@foxmail.com>
2016-05-04 14:22:24 +0800

    add perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -10,31 +10,26 @@ class Perceptron(object):
         self.errors_ = []
 
         for _ in range(self.epochs):
-            errors = 0
             for xi, target in zip(X, y):
                 update = self.eta * (target - self.predict(xi))
                 self.w_[1:] += update * xi
                 self.w_[0] += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
 
         return self
 
-    def new_input(self, x):
-        return np.dot(x, self.w_[1:]) + self.w_[0]
-
     def predict(self, x):
-        return np.where(self.new_input(x) >= 0.0, 1, -1)
+        res = np.dot(x, self.w_[1:]) + self.w_[0]
+        return np.where(res >= 0.0, 1, -1)
 
 def test():
     X = np.array([[3, 3], [4, 3], [1, 1]])
     y = np.array([1, 1, -1])
     x = [1, 0]
 
-    clf = Perceptron()
+    clf = Perceptron(eta = 1)
     clf = clf.train(X, y)
     print clf.predict(x)
-    print clf.errors_
+    print clf.w_
 
 if __name__ == '__main__':
     test()
