58fe431b0cc1554eda17ccd1a3daf4d54a510dfb
rjianwang <rjianwang@foxmail.com>
2016-05-11 00:06:53 +0800

    Neural Network

--- nn.py
+++ nn.py
@@ -153,5 +153,17 @@ class NeuralNetwork(object):
         """
         return (output_activations - y)
 
+    def sigmoid(z):
+        """
+        The sigmoid function.
+        """
+        return 1.0 / (1,0 + np.exp(-z))
+
+    def sigmoid_prime(z):
+        """
+        Derivative of the sigmoid function.
+        """
+        return sigmoid(z) * (1 - sigmoid(z))
+
 if __name__ == '__main__':
     pass
